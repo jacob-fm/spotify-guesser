56d8284c4e9f2ad6da6cb62eeb42d14a9053a9a5
JacobJFM <jacobfeitmann@gmail.com>
2025-04-17 18:49:21 +0300

    Filter function works with artists.json

--- src/components/ArtistPlaceholder.jsx
+++ src/components/ArtistPlaceholder.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
+import artistsData from "../data/artists.json"
 
 export default function ArtistPlaceholder() {
 	const [searchInput, setSearchInput] = useState("");
 
+	// This version uses placeholder user data
+	// const fetchData = (value) => {
+	// 	fetch("https://jsonplaceholder.typicode.com/users")
+	// 		.then((response) => response.json())
+	// 		.then((json) => {
+    //             const results = json.filter((user) => {
+    //                 return user  && user.name && user.name.toLowerCase().includes(value.toLowerCase());
+    //             })
+    //             console.log("Search Results:", results);
+	// 		});
+	// };
+
+	// This version uses dummy artist data from data/artists.json
 	const fetchData = (value) => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((response) => response.json())
-			.then((json) => {
-                const results = json.filter((user) => {
-                    return user  && user.name && user.name.toLowerCase().includes(value.toLowerCase());
-                })
-                console.log("Search Results:", results);
-			});
+		const results = artistsData.filter((artist) => {
+			return artist && artist.name.toLowerCase().includes(value.toLowerCase())
+		})
+		console.log("Search Results: ", results);
 	};
 
 	const handleChange = (value) => {
