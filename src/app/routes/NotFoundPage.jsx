a2bc8264766cf8bb9091229cee0e9a5b6235c936
JacobJFM <jacobfeitmann@gmail.com>
2025-08-14 02:58:49 -0400

    minor 404 page styling

--- src/app/routes/NotFoundPage.jsx
+++ src/app/routes/NotFoundPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-import Header from '/src/components/Header/Header';
+import Header from "/src/components/Header/Header";
 
 export default function NotFoundPage() {
-	return (
-		<>
-            <Header />
-			<div className="not-found">
-				404 Not Found
-				<Link to="/">Home</Link>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <div className="not-found">
+        <h2>404 Error</h2>
+        <p>Sorry, that page doesn't seem to exist.</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </>
+  );
 }
