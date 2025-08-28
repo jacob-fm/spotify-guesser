d8a1c2263cb606a13774518eb4696e20a9a531f9
JacobJFM <jacobfeitmann@gmail.com>
2025-08-28 17:46:12 -0400

    fix reset password page design

--- src/app/routes/AccountForms/ResetPassword.jsx
+++ src/app/routes/AccountForms/ResetPassword.jsx
@@ -2,46 +2,53 @@ import { useState } from "react";
 import { UserAuth } from "/src/lib/AuthContext";
 import Header from "/src/components/Header/Header";
 import { useNavigate } from "react-router-dom";
+import "./AccountForms.css";
 
 export default function ResetPassword() {
-	const [password, setPassword] = useState("");
-	const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-	const { session, updatePassword } = UserAuth();
-	const navigate = useNavigate();
+  const { session, updatePassword } = UserAuth();
+  const navigate = useNavigate();
 
-	const handleResetPassword = async (e) => {
-		e.preventDefault();
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
 
-		try {
-			const result = await updatePassword(password);
-			if (result.success) {
-				navigate("/dashboard");
-			} else {
-				setError(result.error.message);
-			}
-		} catch (err) {
-			setError(err.message);
-		}
-	};
+    try {
+      const result = await updatePassword(password);
+      if (result.success) {
+        navigate("/dashboard");
+      } else {
+        setError(result.error.message);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
-	return (
-		<>
-			<Header />
-			<div>
-				<form onSubmit={handleResetPassword}>
-					<h2>Reset Password</h2>
-					<div>
-						<input
-							onChange={(e) => setPassword(e.target.value)}
-							placeholder="New password"
-							type="password"
-						/>
-						<button type="submit">Set new password</button>
-					</div>
-				</form>
-				{error && <p>{error}</p>}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <div className="account-forms">
+        <h2>Reset Password</h2>
+        <form onSubmit={handleResetPassword}>
+          <section>
+            <div className="input-group">
+              <span className="label">New Password</span>
+              <input
+                id="new-password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter new password"
+                type="password"
+              />
+            </div>
+            <button type="submit" className="filled-alt">
+              Set new password
+            </button>
+          </section>
+        </form>
+        {error && <p>{error}</p>}
+      </div>
+    </>
+  );
 }
