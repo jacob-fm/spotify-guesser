90f5f3a5fb411e37c47c4ee410144f874353a5a9
JacobJFM <jacobfeitmann@gmail.com>
2025-09-02 20:25:06 -0400

    check supabase for todays game in UnplayedRoute

--- src/components/UnplayedRoute.jsx
+++ src/components/UnplayedRoute.jsx
@@ -1,9 +1,17 @@
 import { Navigate } from "react-router-dom";
+import { UserAuth } from "../lib/AuthContext";
 
 const UnplayedRoute = ({ children }) => {
+  const { todaysGameExistsInSupabase } = UserAuth();
   const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
   const playedToday = localStorage.getItem("lastDateCompleted") === today;
-  return <>{!playedToday ? <>{children}</> : <Navigate to="/dashboard" />}</>;
+  return (
+    <>
+      {(!playedToday && !todaysGameExistsInSupabase)
+        ? <>{children}</>
+        : <Navigate to="/dashboard" />}
+    </>
+  );
 };
 
 export default UnplayedRoute;
